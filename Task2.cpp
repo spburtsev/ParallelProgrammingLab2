6eeaf060370dcc5e2de17a0c6ed59a50bc3fa5a4
Sergei Burtsev <spburtsev@gmail.com>
2021-11-14 18:32:55 +0200

    Arr1 added in task# 2

--- Task2.cpp
+++ Task2.cpp
@@ -8,9 +8,15 @@ typedef std::chrono::high_resolution_clock::time_point timePoint;
 
 void task2()
 {
-	std::array<int, 1000> arr {};
+	constexpr int ARR_SIZE = 10000;
+
+	std::array<int, ARR_SIZE> arr {};
 	for (auto& el : arr)
 		el = rand();
+
+	std::array<int, ARR_SIZE> arr1 {};
+	for (size_t i = 0; i < arr.size(); ++i)
+		arr1[i] = arr[i];
 
 	timePoint begin;
 	timePoint end;
@@ -21,14 +27,14 @@ void task2()
 	sort(arr);
 	end = std::chrono::high_resolution_clock::now();
 	std::cout << "\t"
-		<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
 		<< std::endl;
 
 	begin = std::chrono::high_resolution_clock::now();
-	optSort(arr);
+	optSort(arr1);
 	end = std::chrono::high_resolution_clock::now();
 	std::cout << "\t"
-		<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
 		<< std::endl;
 
 	printDelimeter();
