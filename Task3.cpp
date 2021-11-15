a00ab92d9c98a954a7a4c371ab1281a798a78b81
Sergei Burtsev <spburtsev@gmail.com>
2021-11-15 20:20:27 +0200

    Task3 minor refactoring

--- Task3.cpp
+++ Task3.cpp
@@ -4,7 +4,7 @@
 void task3()
 {
 	std::array<int, 3> arr1 { rand() % 100, rand() % 100, rand() % 100 };
-	std::array<int, 3> arr2 { -1, 0, 1 };
+	std::array<int, 4> arr2 { -1, 0, 1, 1 };
 
 	auto product = multiply(arr1, arr2);
 
